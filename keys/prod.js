e6a4d585d65193af0d9eb52ce8d87a33f53f6e1b
mazenongithub <mazen@egeotechnical.com>
2024-08-11 12:53:25 -0700

    updated prod js

---
+++ keys/prod.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    DATABASE: process.env.DATABASE,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    SERVER: process.env.SERVER,
+    MONGODB: process.env.MONGODB,
+    SECRETKEY: process.env.SECRETKEY,
+    HOSTNAME: process.env.HOSTNAME,
+    DB: process.env.DB,
+    USERNAME:process.env.USERNAME,
+    DBPASS:process.env.DBPASS
+}
